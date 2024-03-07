ceb641622abc50023d8d4a8ac8c2eac6f66ed31c
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-07 08:27:27 +0400

    About page design + html

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,32 @@
 import Image from "next/image"
-import AboutImage from "../../../public/about.png"
 import styles from "./about.module.css"
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subTitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>We</p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src={AboutImage} fill alt="" priority />
+        <Image src="/about.png" fill alt="About Image" priority className={styles.img} />
       </div>
     </div>
   )
