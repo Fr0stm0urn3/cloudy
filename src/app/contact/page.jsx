7f1e4d2641eb1c66bfb972cc3002d62a17efd3a7
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-07 09:01:23 +0400

    Contact page design

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image"
+import styles from "./contact.module.css"
+import contactImg from "../../../public/contact (1).png"
+
 const ContactPage = () => {
-  return <div>ContactPage</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={contactImg} alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" className={styles.input} placeholder="Name and Surname" />
+          <input type="text" className={styles.input} placeholder="Email Address" />
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Phone Number (Optional)"
+          />
+          <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default ContactPage
